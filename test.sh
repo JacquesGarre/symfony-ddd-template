40578604d8af0b5436349680501ce7e47b0aea52
Jaks <jcqs.gr@gmail.com>
2025-03-08 18:18:08 +0100

    integration tests

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec -it php php  vendor/bin/phpstan analyse src tests --level 10 -c phpstan.neon
+docker exec -it php php vendor/bin/phpstan analyse src tests --level 10 -c phpstan.neon
 docker exec -it php php vendor/bin/phpunit -c tests/phpunit.unit.xml.dist
 docker exec -it php php vendor/bin/phpunit -c tests/phpunit.integration.xml.dist
