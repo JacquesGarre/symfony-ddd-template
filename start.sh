e4b70eb0e43485deefabd08cf159446af6216750
Jacques Garré <jcqs.gr@gmail.com>
2025-03-09 11:17:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,21 +11,18 @@ composer install
 # Clear Symfony cache
 echo "🧹 Clearing cache..."
 php bin/console cache:clear
+php bin/console cache:warmup
 
 # Build and start Docker containers
 echo "🚀 Building and starting containers..."
 docker compose build
-docker compose up -d
 
 # Run Symfony commands inside the PHP container
 PHP_CONTAINER_NAME="php" 
 
 echo "🔄 Running Symfony commands in the container..."
 docker exec -it $PHP_CONTAINER_NAME php bin/console cache:clear
+docker exec -it $PHP_CONTAINER_NAME php bin/console cache:warmup
 docker exec -it $PHP_CONTAINER_NAME php bin/console doctrine:migrations:migrate --no-interaction
 
-# Display success message
-echo "✔ App restarted and running at http://localhost:8000"
-
-# Optional: Watch for changes (if supported)
-docker compose watch
+docker compose up --watch
